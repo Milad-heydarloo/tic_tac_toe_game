cc926638bd87d5ce93de26ccf460bed079bcfb38
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-24 03:26:16 -0700

    start new project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_application());
+}
+
+class _application extends StatefulWidget {
+  const _application({super.key});
+
+  @override
+  State<_application> createState() => _aApplicationState();
+}
+
+class _aApplicationState extends State<_application> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
